5146fc472a4ca6663344952eebb7de316698b420
JLDelz <102981174+JLDelz@users.noreply.github.com>
2022-04-04 19:30:28 +0200

    Update Reddit Place - Armée de Kameto.user.js

--- Reddit Place - Armée de Kameto.user.js
+++ Reddit Place - Armée de Kameto.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name         Reddit Place - Armée de Kameto
+// @name         Reddit Place - France X One piece X KC X JL
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  On va récuperer ce qui nous est du de droit.
-// @author       Adcoss95
+// @author       Feneck
 // @match        https://hot-potato.reddit.com/embed*
-// @icon         https://styles.redditmedia.com/t5_4eiiz1/styles/communityIcon_ojy24r8j90o81.jpg
+// @icon         https://i.imgur.com/ahw2OTX.jpg
 // @grant        none
 // ==/UserScript==
 if (window.top !== window.self) {
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://raw.githubusercontent.com/JLDelz/JOBLESS/main/unknown%20(1).png";
+            i.src = "https://i.imgur.com/VL5lRJb.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
             console.log(i);
             return i;
